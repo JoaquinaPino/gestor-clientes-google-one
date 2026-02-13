4f80195376f2da995ce3f4e3ae07064d329e9952
joaquinapino <joaquina.pino@mail.udp.cl>
2026-02-12 23:45:31 -0300

    Implementación de super clase Plan y subclases, actualización UML

---
+++ modelos/plan.py
@@ -0,0 +1,98 @@
+class Plan:
+    def __init__(self, nombre, precio, almacenamiento_gb):
+        self._nombre = nombre
+        self._precio = precio
+        self._almacenamiento_gb = almacenamiento_gb
+        self.__suscripcion = None
+
+    # --- GETTERS Y SETTERS ---
+
+    def get_precio(self):
+        return self._precio
+    
+    def set_precio(self, nuevo_precio):
+        if nuevo_precio < 0:
+            print("Error: El precio no puede ser negativo")
+        else:
+            self._precio = nuevo_precio
+
+    def get_nombre(self):
+        return self._nombre
+    
+    def get_almacenamiento(self):
+        return self._almacenamiento_gb
+    
+    # --- MÉTODOS DE RELACIÓN ---
+    
+    def asignar_suscripcion(self, suscripcion):
+        self.__suscripcion = suscripcion
+
+    def obtener_suscripcion(self):
+        return self.__suscripcion
+    
+    # --- MÉTODOS DE LÓGICA ---
+
+    def verificar_almacenamiento(self, gb_uso):
+        return gb_uso <= self._almacenamiento_gb
+    
+    def calcular_costo_total(self):
+        return self._precio
+    
+    def listar_beneficios(self):
+        raise NotImplementedError("Este error debe ser implementado por las clases hijas")
+
+class PlanGratuito(Plan):
+    def __init__(self):
+        super().__init__("Google Free", 0.0, 15)
+
+    def listar_beneficios(self):
+        return ["Google Free", "15GB Drive", "Google Photos"]
+    
+class PlanPremium(Plan):
+    def __init__(self):
+        super().__init__("Google One AI Premium", 19.99, 2048)
+        self.__acceso_gemini = True
+
+    def get_acceso_gemini(self):
+        return self.__acceso_gemini
+    
+    def listar_beneficios(self):
+        return [
+            "2TB Almacenamiento",
+            "Gemini Advanced",
+            "Magic Eraser"
+        ]
+    
+class PlanEmpresa(Plan):
+    def __init__(self, cantidad_usuarios, dominio_personalizado):
+        super().__init__("Google Enterprise", 10.0, 100000)
+        self.__cantidad_usuarios = cantidad_usuarios
+        self.__dominio_personalizado = dominio_personalizado
+
+    # --- GETTERS Y SETTERS ESPECIFICOS EMPRESA ---
+    def get_cantidad_usuarios(self):
+        return self.__cantidad_usuarios
+    
+    def set_cantidad_usuarios(self, n):
+        if n < 3:
+            print("Error: Deber haber al menos 3 usuarios")
+        else:
+            self.__cantidad_usuarios = n
+
+    def get_dominio(self):
+        return self.__dominio_personalizado
+    
+    # --- Polimorfismo ---
+    def calcular_costo_total(self):
+        return self._precio * self.__cantidad_usuarios
+    
+    def listar_beneficios(self):
+        return [
+            f"Espacio compartido para {self.__cantidad_usuarios} usuarios",
+            f"Dominio @{self.__dominio_personalizado}",
+            "Soporte 24/7"
+        ]
+    
+    
+
+    
